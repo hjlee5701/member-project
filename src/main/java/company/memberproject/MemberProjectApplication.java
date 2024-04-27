3cac548f5b04db9fc0378914cd7ec2cb100c4696
hjlee5701 <hjlee5701@gmail.com>
2024-04-27 20:46:26 +0900

    Initial commit

---
+++ src/main/java/company/memberproject/MemberProjectApplication.java
@@ -0,0 +1,13 @@
+package company.memberproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/company/memberproject/MemberProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package company.memberproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
